7fabcd0002feec238640009f5e818ceb6327c70d
dora-explora <sarrafdara@gmail.com>
2025-07-08 19:59:55 -0700

    made levels actually replayable

--- chordfall.py
+++ chordfall.py
@@ -1,7 +1,5 @@
 import pygame
 import pygame_menu
-from time import sleep
-# import threading
 
 import pygame_menu.themes
 
@@ -15,14 +13,6 @@ class Player(pygame.sprite.Sprite):
         frames = ["playerA.bmp", "playerB.bmp"]
         self.image = pygame.image.load(frames[frame]).convert_alpha()
 
-def bounce_character(player: pygame.sprite.Sprite):
-    frame = 0
-    while True:
-        player.update(frame)
-        frame += 1
-        frame %= 2
-        sleep(0.5)
-
 class Note(pygame.sprite.Sprite):
     def __init__(self, white_note: bool, height: int, x: float, y: float):
         super().__init__()
@@ -35,6 +25,12 @@ class Note(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect = self.rect.move(x + 5, y - height)
 
+def construct_notes(list) -> list[Note]:
+    notes: list[Note] = []
+    for note in list:
+        notes.append(Note(note[0], note[1], note[2], note[3]))
+    return notes
+
 pygame.init()
 screen = pygame.display.set_mode((720, 900))
 clock = pygame.time.Clock()
@@ -44,11 +40,11 @@ LEVEL = [0, 0] # level number and frame countdown
 level_start_event = pygame.event.Event(pygame.USEREVENT)
 
 player = Player()
-# bouncer = threading.Thread(target=bounce_character, args=[player])
-# bouncer.start()
 position = 330
 move_speed = 4
 note_speed = 10
+player_frame = 0
+player_bounce_countdown = 40
 
 # note -> x position key:
 C4 = 8
@@ -66,36 +62,36 @@ G5 = 668
 
 level_1_bar_length = 375  # in pixels
 level_1_offset = -650
-level_1_notes = [
+level_1_notelist = [
                 # Fmaj7
-                Note(True, level_1_bar_length, F4, level_1_offset), 
-                Note(True, level_1_bar_length, A4, level_1_offset), 
-                Note(True, level_1_bar_length, C5, level_1_offset), 
-                Note(True, level_1_bar_length, E5, level_1_offset),
+                [True, level_1_bar_length, F4, level_1_offset], 
+                [True, level_1_bar_length, A4, level_1_offset], 
+                [True, level_1_bar_length, C5, level_1_offset], 
+                [True, level_1_bar_length, E5, level_1_offset],
                 # Em7
-                Note(True, level_1_bar_length, E4, -level_1_bar_length * (3 + 2/3) + level_1_offset), 
-                Note(True, level_1_bar_length, G4, -level_1_bar_length * (3 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, B4, -level_1_bar_length * (3 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, D5, -level_1_bar_length * (3 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, E5, -level_1_bar_length * (3 + 2/3) + level_1_offset),
+                [True, level_1_bar_length, E4, -level_1_bar_length * (3 + 2/3) + level_1_offset], 
+                [True, level_1_bar_length, G4, -level_1_bar_length * (3 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, B4, -level_1_bar_length * (3 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, D5, -level_1_bar_length * (3 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, E5, -level_1_bar_length * (3 + 2/3) + level_1_offset],
                 # Dm9
-                Note(True, level_1_bar_length, D4, -level_1_bar_length * (8) + level_1_offset),
-                Note(True, level_1_bar_length, F4, -level_1_bar_length * (8) + level_1_offset),
-                Note(True, level_1_bar_length, A4, -level_1_bar_length * (8) + level_1_offset),
-                Note(True, level_1_bar_length, C5, -level_1_bar_length * (8) + level_1_offset),
-                Note(True, level_1_bar_length, E5, -level_1_bar_length * (8) + level_1_offset),
+                [True, level_1_bar_length, D4, -level_1_bar_length * (8) + level_1_offset],
+                [True, level_1_bar_length, F4, -level_1_bar_length * (8) + level_1_offset],
+                [True, level_1_bar_length, A4, -level_1_bar_length * (8) + level_1_offset],
+                [True, level_1_bar_length, C5, -level_1_bar_length * (8) + level_1_offset],
+                [True, level_1_bar_length, E5, -level_1_bar_length * (8) + level_1_offset],
                 # G7/D
-                Note(True, level_1_bar_length, D4, -level_1_bar_length * (9 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, F4, -level_1_bar_length * (9 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, G4, -level_1_bar_length * (9 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, B4, -level_1_bar_length * (9 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, D5, -level_1_bar_length * (9 + 2/3) + level_1_offset),
+                [True, level_1_bar_length, D4, -level_1_bar_length * (9 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, F4, -level_1_bar_length * (9 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, G4, -level_1_bar_length * (9 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, B4, -level_1_bar_length * (9 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, D5, -level_1_bar_length * (9 + 2/3) + level_1_offset],
                 # Cmaj7
-                Note(True, level_1_bar_length, C4, -level_1_bar_length * (11 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, E4, -level_1_bar_length * (11 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, G4, -level_1_bar_length * (11 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, B4, -level_1_bar_length * (11 + 2/3) + level_1_offset),
-                Note(True, level_1_bar_length, E5, -level_1_bar_length * (11 + 2/3) + level_1_offset)
+                [True, level_1_bar_length, C4, -level_1_bar_length * (11 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, E4, -level_1_bar_length * (11 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, G4, -level_1_bar_length * (11 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, B4, -level_1_bar_length * (11 + 2/3) + level_1_offset],
+                [True, level_1_bar_length, E5, -level_1_bar_length * (11 + 2/3) + level_1_offset]
                 ]
 
 def level_1():
@@ -106,7 +102,7 @@ def level_1():
     LEVEL[1] = 860
     pygame.event.post(level_start_event)
 
-levels = [level_1_notes]
+levels_notelists = [level_1_notelist]
 level_countdown = 0
 notes: list[Note] = []
 
@@ -115,25 +111,36 @@ piano = pygame.image.load("piano.bmp")
 menu = pygame_menu.Menu("Chordfall", 400, 500, theme=pygame_menu.themes.THEME_DARK)
 menu.add.button("Level 1", level_1)
 
-while running:
+while True:
+    # check events to handle quitting and level start
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
+            pygame.quit()
             running = False
         if event.type == pygame.USEREVENT:
-            notes = levels[LEVEL[0] - 1].copy()
-
+            notes = construct_notes(levels_notelists[LEVEL[0] - 1])
+    if running == False:
+        break
+    # fill background with sky blue first
+    screen.fill((16, 185, 204))
+    # handle movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         position -= move_speed
     elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         position += move_speed
-
-    screen.fill((16, 185, 204))
+    # render character and manage bouncing
+    player_bounce_countdown -= 1
+    if player_bounce_countdown == 0:
+        player_bounce_countdown = 40
+        player_frame += 1
+        player_frame %= 2
+        player.update(player_frame)
     screen.blit(player.image, (position, 700), )
-
+    # all the in-level logic
     if LEVEL[0] != 0:
-        LEVEL[1] -= 1
+        LEVEL[1] -= 1 # decrement the level length countdown
         for note in notes:
             note.rect = note.rect.move(0, note_speed)
             screen.blit(note.image, note.rect)
@@ -143,8 +150,8 @@ while running:
             pygame.mixer.music.play()
         if LEVEL[1] == 0:
             LEVEL[0] = 0
+            pygame.mixer.music.fadeout(1500)
             menu.enable()
-
 
     screen.blit(piano, (0, 750))
 
@@ -155,6 +162,3 @@ while running:
 
     pygame.display.flip()
     clock.tick(60)  # limits FPS to 60, apparently
-
-
-pygame.quit()
