a49e953bac010a2c3e506bdb6206f72335656f1d
dora-explora <sarrafdara@gmail.com>
2025-07-10 23:01:55 -0700

    added light-up keys and finished score system

--- chordfall.py
+++ chordfall.py
@@ -72,7 +72,14 @@ levels_notelists = [notes.level_1_notelist, notes.level_2_notelist] # type: igno
 level_countdown = 0
 notes: list[Note] = []
 
-piano = pygame.image.load("piano.bmp")
+background = pygame.image.load("background.bmp")
+piano = pygame.image.load("piano.bmp").convert_alpha()
+left_key = pygame.image.load("left_key.bmp").convert_alpha()
+middle_key = pygame.image.load("middle_key.bmp").convert_alpha()
+right_key = pygame.image.load("right_key.bmp").convert_alpha()
+black_key = pygame.Surface((30, 75))
+black_key.fill((99, 198, 77))
+render_keys: list[tuple[pygame.Surface, tuple[int, int]]] = []
 
 menu = pygame_menu.Menu("Chordfall", 300, 375, theme=pygame_menu.themes.THEME_DARK)
 menu.add.button("Level 1", level_1)
@@ -92,11 +99,12 @@ while True:
             pygame.quit()
             running = False
         if event.type == pygame.USEREVENT:
+            score = 0
             notes = construct_notes(levels_notelists[LEVEL[0] - 1])
     if running == False:
         break
-    # fill background with sky blue first
-    screen.fill((16, 185, 204))
+    # render
+    screen.blit(background, (0, 0))
     # handle movement
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
@@ -117,6 +125,7 @@ while True:
     if LEVEL[0] != 0:
         LEVEL[1] -= 1 # decrement the level length countdown
         if not game_over:
+            render_keys = []
             for note in notes:
                 note.rect = note.rect.move(0, note_speed)
                 screen.blit(note.image, note.rect)
@@ -124,18 +133,59 @@ while True:
                     game_over = True
                 elif pygame.Rect(position + 20, 720, 20, 45).colliderect(note.rect) or pygame.Rect(position - 20, 720, 20, 45).colliderect(note.rect):
                     score += 20
-                    print(f"near miss! score = {score}")
+                    # print(f"near miss! score = {score}")
                 elif pygame.Rect(position + 40, 720, 20, 45).colliderect(note.rect) or pygame.Rect(position - 40, 720, 20, 45).colliderect(note.rect):
                     score += 10
-                    print(f"close! score = {score}")
+                    # print(f"close! score = {score}")
                 elif pygame.Rect(position + 60, 720, 20, 45).colliderect(note.rect) or pygame.Rect(position - 60, 720, 20, 45).colliderect(note.rect):
                     score += 5
-                    print(f"about, score = {score}")
-        if game_over == True:
+                    # print(f"about, score = {score}")
+                
+                if note.rect.collidepoint(30, 750):
+                    render_keys.append((left_key, (8, 758)))
+                elif note.rect.collidepoint(60, 750):
+                    render_keys.append((black_key, (45, 758)))
+                elif note.rect.collidepoint(90, 750):
+                    render_keys.append((middle_key, (68, 758)))
+                elif note.rect.collidepoint(120, 750):
+                    render_keys.append((black_key, (105, 758)))
+                elif note.rect.collidepoint(150, 750):
+                    render_keys.append((right_key, (128, 758)))
+                elif note.rect.collidepoint(210, 750):
+                    render_keys.append((left_key, (188, 758)))
+                elif note.rect.collidepoint(240, 750):
+                    render_keys.append((black_key, (225, 758)))
+                elif note.rect.collidepoint(270, 750):
+                    render_keys.append((middle_key, (248, 758)))
+                elif note.rect.collidepoint(300, 750):
+                    render_keys.append((black_key, (285, 758)))
+                elif note.rect.collidepoint(330, 750):
+                    render_keys.append((middle_key, (308, 758)))
+                elif note.rect.collidepoint(360, 750):
+                    render_keys.append((black_key, (345, 758)))
+                elif note.rect.collidepoint(390, 750):
+                    render_keys.append((right_key, (368, 758)))
+                elif note.rect.collidepoint(450, 750):
+                    render_keys.append((left_key, (428, 758)))
+                elif note.rect.collidepoint(480, 750):
+                    render_keys.append((black_key, (465, 758)))
+                elif note.rect.collidepoint(510, 750):
+                    render_keys.append((middle_key, (488, 758)))
+                elif note.rect.collidepoint(540, 750):
+                    render_keys.append((black_key, (525, 758)))
+                elif note.rect.collidepoint(570, 750):
+                    render_keys.append((right_key, (548, 758)))
+                elif note.rect.collidepoint(630, 750):
+                    render_keys.append((left_key, (608, 758)))
+                elif note.rect.collidepoint(660, 750):
+                    render_keys.append((black_key, (645, 758)))
+                elif note.rect.collidepoint(690, 750):
+                    render_keys.append((right_key, (668, 758)))
+        if game_over:
+            render_keys = []
             LEVEL[1] = 0
             print("game over!")
 
-        
         if LEVEL[1] == 100:
             pygame.mixer.music.load("golf clap.mp3")
             pygame.mixer.music.play()
@@ -151,6 +201,10 @@ while True:
             menu.enable()
 
     screen.blit(piano, (0, 750))
+    for key in render_keys:
+        screen.blit(key[0], key[1])
+    score_text = pygame.font.Font(None, 32).render(f"Score: {score}", True, "black")
+    screen.blit(score_text, (10, 10))
 
     if menu.is_enabled():
         menu.update(events)
